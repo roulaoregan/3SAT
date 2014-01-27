9b10acf7e4be9b6d0dfc0f1cceecda2a665b3290
roulaoregan <roula.oregan@gmail.com>
2014-01-26 22:07:12 -0800

    Create execption_file_handling.py
    Subclass to handle file exceptions

---
+++ execption_file_handling.py
@@ -0,0 +1,24 @@
+'''
+ copyright 2014
+ author: Spiridoula O'Regan
+ email: roula.oregan@gmail.com
+ github user: roulaoregan
+'''
+import logger
+import math
+import os
+import random
+import re
+import sys
+
+from collections import Counter
+from optparse import OptionParser
+
+'''
+Handles file input errors
+'''
+class FileInputError(Exception):
+        def __init__(self, value):
+                self.value = value
+        def __str__(self):
+                return repr(self.value)
