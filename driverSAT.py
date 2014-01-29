0daf1dc44b3b37dc81e692c1658d0847d528be2a
roulaoregan <roula.oregan@gmail.com>
2014-01-28 21:52:53 -0800

    Update driverSAT.py
    code cleanup

--- driverSAT.py
+++ driverSAT.py
@@ -13,23 +13,45 @@ import sys
 
 from collections import Counter
 from optparse import OptionParser
-
+from literals import Literals
+from dimacs_parser import DimacsParser
+from sat import SAT
 
 def main(argv):
-        logging.basicConfig(filename='dpll_log.log', level=logging.INFO)
+	logger = logging.getLogger('dpll_log')
+	logger.setLevel(logging.DEBUG)
 
-        arg_handler = OptionParser("DPLL 3 SAT Solver")
-        arg_handler.add_option('d', '--debug', type='string', help='debug levels Critical (3), Error (2), Warning (1), info (0), debug (-1), default='CRITICAL')    
-        arg_handler.add_option('p', '--printtostdout', action='store_true', default=False, help='Print all log message to stdout')
-        options, args = arg_handler.parse_args()
+	fh = logging.FileHandler("dpll.log")
+	fh.setLevel(logging.DEBUG)
+	
+	ch = logging.StreamHandler()
+	ch.setLevel(logging.DEBUG)
 
-        #Parse DCIMACS file
-        parse_file = DimacsParser(argv[0])
-        parse_file.parse()
-        (clauses, symbols, model) = parse_file.sentences()
+	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	fh.setFormatter(formatter)
+	ch.setFormatter(formatter)
+	logger.addHandler(fh)
+	logger.addHandler(ch)
 
-        satisfiable = dpll(clauses, symbols, model)
+	arg_handler = OptionParser("DPLL 3 SAT Solver")
+	arg_handler.add_option('d', '--debug', type='string', help='debug and log info and errors')    
+	arg_handler.add_option('p', '--printtostdout', action='store_true', default=False, help='Print all log message to stdout')
+	options, args = arg_handler.parse_args()
+
+	#Parse Dimacs file
+	parse_file = DimacsParser(argv[0])
+	parse_file.parse()
+	(clauses, literals, model) = parse_file.sentences()
+	symbols = Literals(literals)
+	logger.info("parsed Dimacs file")
+	logger.info("clauses: %s"%(clauses))
+	logger.info("created Literals() and add symbols: %s"%(symbols))
+	logger.info("model: %s"%(model))
+	#call SAT Solver
+	solver = SAT(clauses, symbols, model)
+	satisfiable = solver.satisfiable(clauses, symbols, model)
 
 
 if __name__ = "__main__":
-        main(sys.argv[1:])
+	main(sys.argv[1:])
+
