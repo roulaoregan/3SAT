c42c020f1a15249c24d9fed46c005b2aade601b0
roulaoregan <roula.oregan@gmail.com>
2014-01-28 22:25:30 -0800

    Update driverSAT.py
    bug fixes
    todo: finish arg handlers

--- driverSAT.py
+++ driverSAT.py
@@ -13,9 +13,17 @@ import sys
 
 from collections import Counter
 from optparse import OptionParser
-from literals import Literals
 from dimacs_parser import DimacsParser
 from sat import SAT
+
+'''
+Handles file input errors
+'''
+class FileInputError(Exception):
+        def __init__(self, value):
+                self.value = value
+        def __str__(self):
+                return repr(self.value)
 
 def main(argv):
 	logger = logging.getLogger('dpll_log')
@@ -34,26 +42,22 @@ def main(argv):
 	logger.addHandler(ch)
 
 	arg_handler = OptionParser("DPLL 3 SAT Solver")
-	arg_handler.add_option('d', '--debug', type='string', help='debug and log info and errors')    
+	#arg_handler.add_option('d', '--debug', type='string', help='debug and log info and errors')    
 	#arg_handler.add_option('p', '--printtostdout', action='store_true', default=False, help='Print all log message to stdout')
-	options, args = arg_handler.parse_args()
+	#options, args = arg_handler.parse_args()
 
 	#Parse Dimacs file
 	parse_file = DimacsParser(argv[0])
 	parse_file.parse()
-	(clauses, literals, model) = parse_file.sentences()
-	symbols = Literals(literals)
-	
+	(clauses, symbols, model) = parse_file.sentences()
 	logger.debug("parsed Dimacs file")
 	logger.debug("clauses: %s"%(clauses))
 	logger.debug("created Literals() and add symbols: %s"%(symbols))
 	logger.debug("model: %s"%(model))
-	
 	#call SAT Solver
 	solver = SAT(logger=logger, clauses=clauses, symbols=symbols, model=model)
 	logger.debug("calling SAT solver")
 	satisfiable = solver.satisfiable()
-
 
 
 if __name__ == "__main__":
