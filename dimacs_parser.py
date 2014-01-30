7e74fb3daa225b585c62791014c270540b5e88f8
roulaoregan <roula.oregan@gmail.com>
2014-01-29 21:28:05 -0800

    Update dimacs_parser.py
    bug fixes

--- dimacs_parser.py
+++ dimacs_parser.py
@@ -21,33 +21,43 @@ TODO: change "print" comments to logger!
 
 '''
 class DimacsParser(object):
-        def __init__(self, filepath):
+        def __init__(self, filepath, log):
                 self.file_path = filepath
                 self.clauses = []
                 self.symbols = Literals()
                 self.model = {}
+                self.log = log
 
         def parse(self):
+                
                 symbol_list = []
                 if self.file_path:
                         with open(self.file_path) as data:
                                 data_lines = (line.rstrip("\r\n") for line in data)
                                 for line in data_lines:
-                                        match_comment = re.match("^p", line)
+                                        match_comment = re.match("^p", line)                                        
                                         match_clause = re.match("^[0-9]|^\-", line)
                                         split_lines = line.rsplit(" ")
 
-                                        if match_clause:
+                                        if match_comment:
                                                 print "number of clauses: %s and number of variables: %s" % (split_lines[-1], split_lines[-2])
-                                        if match_clause:
-                                                self.clauses.append([int(split_lines[0]) + int(split_lines[1]) + int(split_lines[2])])
-                                                symbol_list.extend(self.clauses[-1])
-                                                print "adding clause: %s"%self.clauses[-1]
+                                                self.log.debug("number of clauses: %s and number of variables: %s" % (split_lines[-1], split_lines[-2]))
+                                        
+                                        if match_clause:                                                
+                                                clause = [split_lines[x] for x in range(3)]
+                                                self.log.debug("adding clause: %s"%clause)
+                                                self.clauses.append(clause)
+                                                symbol_list.extend(clause)    
 
-                        [self.symbol.add(symbol) for symbol in symbol_list]
+                        [self.symbols.add(symbol) for symbol in symbol_list]
 
 
         def sentences(self):
+                
                 self.model = [{'clause':cl, 'original':cl, 'conflict': []} for cl in self.clauses]
+                self.log.debug("clauses: %s"%self.clauses)
+                self.log.debug("symbols: %s"%self.symbols.literals)
+                self.log.debug("model: %s"%self.model)
+                
                 return (self.clauses, self.symbols, self.model)
 
